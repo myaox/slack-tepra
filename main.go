e50b72306a92494f990b29d53eda71ba01eedde7
yutaro <llgoodrr@gmail.com>
2017-10-08 10:12:10 +0900

    UPDATE loadconfig

--- main.go
+++ main.go
@@ -2,25 +2,41 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
+	"path/filepath"
 
-	"github.com/joho/godotenv"
+	"github.com/BurntSushi/toml"
 	"github.com/nlopes/slack"
 )
 
-func Env_load() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+type Config struct {
+	TOKEN string
+}
+
+func LoadToml(path string) *Config {
+	var conf Config
+	_, err := toml.DecodeFile(path, &conf)
+	if err == nil {
+		return &conf
 	}
+
+	basepath, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		panic(err)
+	}
+	fullpath := filepath.Join(basepath, path)
+
+	_, err = toml.DecodeFile(fullpath, &conf)
+	if err != nil {
+		panic(err)
+	}
+	return &conf
 }
 
 func main() {
-	Env_load()
+	conf := LoadToml("config.toml")
 
-	token := os.Getenv("TOKEN")
-	api := slack.New(token)
+	api := slack.New(conf.TOKEN)
 	rtm := api.NewRTM()
 	go rtm.ManageConnection()
 
