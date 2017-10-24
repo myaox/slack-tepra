247bf260bdc40923ecb58897076275285946a397
yutaro <llgoodrr@gmail.com>
2017-10-24 15:10:12 +0900

    change file set

--- main.go
+++ main.go
@@ -1,73 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"path/filepath"
-
-	"github.com/BurntSushi/toml"
-	"github.com/nlopes/slack"
+	"github.com/yutaro/slack-cmd-go"
 )
 
-type Config struct {
-	TOKEN string
-}
-
-func LoadToml(path string) *Config {
-	var conf Config
-	_, err := toml.DecodeFile(path, &conf)
-	if err == nil {
-		return &conf
-	}
-
-	basepath, err := filepath.Abs(filepath.Dir(os.Args[0]))
-	if err != nil {
-		panic(err)
-	}
-	fullpath := filepath.Join(basepath, path)
-
-	_, err = toml.DecodeFile(fullpath, &conf)
-	if err != nil {
-		panic(err)
-	}
-	return &conf
-}
-
 func main() {
-	conf := LoadToml("config.toml")
+	conf := scmd.LoadToml("config.toml")
+	bot := scmd.New(conf.TOKEN)
+	tepra := bot.NewCmds("tepra")
 
-	api := slack.New(conf.TOKEN)
-	rtm := api.NewRTM()
-	go rtm.ManageConnection()
+	tepra.Cmd("print", "print message",
+		func(c *scmd.Context) {
+			c.SendMessage("hello")
+		})
 
-	fmt.Println("--- start connection ---")
-
-Loop:
-	for {
-		select {
-		case msg := <-rtm.IncomingEvents:
-			switch ev := msg.Data.(type) {
-			case *slack.HelloEvent:
-				//
-			case *slack.ConnectedEvent:
-				//
-			case *slack.TeamJoinEvent:
-				//
-			case *slack.MessageEvent:
-				go evalComment(rtm, ev)
-
-			case *slack.ReactionAddedEvent:
-				//
-			case *slack.ReactionRemovedEvent:
-				//
-			case *slack.RTMError:
-				fmt.Println("Err:%s\n", ev.Error())
-				break Loop
-			case *slack.InvalidAuthEvent:
-				fmt.Println("Invalid credentials")
-				break Loop
-			default:
-			}
-		}
-	}
+	bot.Start()
 }
--- slack.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/nlopes/slack"
-)
-
-func reply(rtm *slack.RTM, ev *slack.MessageEvent, mes string) {
-	rtm.SendMessage(rtm.NewOutgoingMessage(mes, ev.Channel))
-}
