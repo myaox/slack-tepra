439f707d75c6b337d500b0956d2ad075da267384
yutaro <llgoodrr@gmail.com>
2017-10-25 00:16:33 +0900

    update

---
+++ csv.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"path"
+	"runtime"
+	"strings"
+
+	"golang.org/x/text/encoding/japanese"
+	"golang.org/x/text/transform"
+)
+
+func urlConv(url string) string {
+	url = strings.Trim(url, "<")
+	url = strings.Trim(url, ">")
+	urls := strings.Split(url, "|")
+
+	return urls[0]
+}
+
+func csvPath(name string) string {
+	return path.Join(getCurPath(), name)
+}
+
+func getCurPath() string {
+	_, filename, _, _ := runtime.Caller(1)
+	return path.Dir(filename)
+}
+
+func writeCsv(args []string) string {
+	csv := csvPath("value.csv")
+	file, _ := os.Create(csv)
+	val := strings.Join(args, ",")
+
+	sjis, _ := utf8ToSjis(val)
+	content := []byte(sjis)
+
+	file.Write(content)
+	file.Close()
+	return csv
+}
+
+func utf8ToSjis(str string) (string, error) {
+	iostr := strings.NewReader(str)
+	rio := transform.NewReader(iostr, japanese.ShiftJIS.NewEncoder())
+	ret, err := ioutil.ReadAll(rio)
+	if err != nil {
+		return "", err
+	}
+	return string(ret), err
+}
--- main.go
+++ main.go
@@ -1,17 +1,70 @@
 package main
 
 import (
+	"strings"
+
 	"github.com/yutaro/slack-cmd-go"
 )
+
+var (
+	templates = map[string]string{
+		"text":    "text.tpe",
+		"text-qr": "text-qr.tpe",
+	}
+)
+
+var exePath = "C:/Program Files (x86)/KING JIM/TEPRA SPC10/SPC10.exe"
 
 func main() {
 	conf := scmd.LoadToml("config.toml")
 	bot := scmd.New(conf.TOKEN)
-	tepra := bot.NewCmds("tepra")
+	tepra := bot.NewCmdGroup("tepra")
 
-	tepra.Cmd("print", "print message",
+	tepra.Cmd("print", []string{"print message"},
 		func(c *scmd.Context) {
-			c.SendMessage("hello")
+			args := c.GetArgs()
+			options := c.GetOptions()
+			mes := strings.Join(args, " ")
+
+			//tpe := templates["text"]
+
+			prints := []string{mes}
+
+			if qr, ok := options["qr"]; ok {
+				url := urlConv(qr)
+				prints = append(prints, url)
+			}
+
+			//csvpath := writeCsv(prints)
+			writeCsv(prints)
+			//cmd := exec.Command("sleep", "5s")
+			//cmd.Start()
+			//cmd.Wait()
+
+			c.SendMessage(mes)
+		})
+
+	tepra.Cmd("qrcode", []string{"print only qrcode"},
+		func(c *scmd.Context) {
+			args := c.GetArgs()
+			if len(args) < 1 {
+				return
+			}
+
+			qr := args[0]
+			url := urlConv(qr)
+
+			c.SendMessage(url)
+		})
+
+	tepra.Cmd("image", []string{"print image"},
+		func(c *scmd.Context) {
+
+		})
+
+	tepra.Cmd("status", []string{"check status"},
+		func(c *scmd.Context) {
+			c.SendMessage("ok")
 		})
 
 	bot.Start()
